525f3716b92ea3c00356c5741bbd73b73ad04808
koltuna <akoltun@pio.carrollu.edu>
2016-11-15 21:39:09 -0600

    Test Commit

--- src/csc640project/CSC640Project.java
+++ src/csc640project/CSC640Project.java
@@ -11,7 +11,7 @@ package csc640project;
  */
 public class CSC640Project {
 //Main class for execution of project
-    //This is a group project simulating ChocAn for CSC640
+    //This is a group project simulating ChocAn for CSC640(Software Engineering)
     /**
      * @param args the command line arguments
      */

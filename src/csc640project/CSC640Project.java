27132d6816eaf209922576559b1bed168b25e37a
Alex <apkoltun@uwm.edu>
2016-11-15 20:53:40 -0600

    Once again testing clone

--- src/csc640project/CSC640Project.java
+++ src/csc640project/CSC640Project.java
@@ -11,7 +11,7 @@ package csc640project;
  */
 public class CSC640Project {
 //Main class for execution of project
-    //This is a group project simulating ChocAn
+    //This is a group project simulating ChocAn for CSC640
     /**
      * @param args the command line arguments
      */
